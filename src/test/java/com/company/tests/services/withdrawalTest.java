9301c5ec6de5b6c72da7ff8f080e7d85005c864d
Elyse Lam <elyse.p.lam@gmail.com>
2019-09-14 12:03:26 -0400

    adding withdrawalTest

--- src/test/java/com/company/tests/services/withdrawalTest.java
+++ src/test/java/com/company/tests/services/withdrawalTest.java
@@ -1,4 +1,27 @@
 package com.company.tests.services;
 
+import com.company.app.models.Account;
+import com.company.app.services.BalanceUpdateService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class withdrawalTest {
+    Account account = new Account();
+
+    BalanceUpdateService balance;
+
+    @Before
+    public void init() {
+        balance = new BalanceUpdateService();
+    }
+
+    @Test
+    public void shouldUpdateBalance() {
+        //starts with 5, withdraw 5, ends up with 0
+        double expectedOutput = 0;
+        account.setBalance(5)
+        Assert.assertEquals("balance not updated", expectedOutput, account.getBalance());
+
+    }
 }
