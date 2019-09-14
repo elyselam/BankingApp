2eb1de5611729d08b1e1cdd24c8e0713b20d1c73
Elyse Lam <elyse.p.lam@gmail.com>
2019-09-14 11:56:11 -0400

    Adding depositTest

--- src/test/java/com/company/tests/services/depositTest.java
+++ src/test/java/com/company/tests/services/depositTest.java
@@ -1,4 +1,27 @@
 package com.company.tests.services;
 
+import com.company.app.models.Account;
+import com.company.app.services.BalanceUpdateService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class depositTest {
+
+    Account account = new Account();
+
+    BalanceUpdateService balance;
+
+    @Before
+    public void init() {
+        balance = new BalanceUpdateService();
+    }
+
+    @Test
+    public void shouldUpdateBalance() {
+        double expectedOutput = 5;
+        account.setBalance(5)
+        Assert.assertEquals("balance not updated", expectedOutput, account.getBalance());
+
+    }
 }
