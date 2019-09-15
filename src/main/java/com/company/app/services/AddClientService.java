02a74f5ee7147725c467ec243aa7de08999931c5
Elyse Lam <elyse.p.lam@gmail.com>
2019-09-14 21:44:14 -0400

    add createAccount() and test

--- src/main/java/com/company/app/services/AddClientService.java
+++ src/main/java/com/company/app/services/AddClientService.java
@@ -1,19 +1,47 @@
 package com.company.app.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 //employee creates account
 //make instance of Account class
 
 import com.company.app.models.Account;
-import com.company.app.models.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class AddClientService {
 
-    public void createCustomer(String email, String password){
-        Customer newCustomer = new Customer(firstName, lastName, email, password);
+    //<email, {id, balance}
+    HashMap<String, Account> clientList= new HashMap();
+
+    public void createAccount(String  email) {
+        Account newClient = new Account();
+        clientList.put(email, newClient);
     }
 
-    public void createAccount() {
-        Account newClient = new Account(id, balance);
 
+    private void createAccount() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AddClientService addAccount = new AddClientService();
+
+        File file = new File("src/resources/data.json");
+        if (!file.exists()) {
+            file.createNewFile();
+
+        } else {
+            try {
+                mapper.writeValue(new FileOutputStream("src/resources/data.json"), clientList);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //LOG.info("account has been added");
+        }
     }
 }
+
---
+++ src/test/java/com/company/tests/services/shouldHaveFileIncreateAccount.java
@@ -0,0 +1,7 @@
+package com.company.tests.services;
+//if file is there and not there
+//file.size is not 0;
+//
+public class shouldHaveFileIncreateAccount {
+    if ()
+}
