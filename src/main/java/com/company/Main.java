3e91fe33cbe65ad8e99d1d94b1e2ad763d308005
Elyse Lam <elyse.p.lam@gmail.com>
2019-09-23 22:41:48 -0400

    adding UserJDBC

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,68 @@
 package com.company;
 
+import com.company.app.dao.UserJDBCDao;
+import com.company.app.dao.UserRepository;
+import com.company.app.models.Users;
 import com.company.app.system.BankApplication;
+import com.company.app.system.ConnectionManager;
+import com.company.app.system.PostgresConnectionManager;
 import com.company.platform.Application;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 
 public class Main {
+    public static ConnectionManager manager;
 
     public static void main(String[] args) {
-        Application app = new BankApplication();
+//        Application app = new BankApplication();
+//        app.run(args);
 
-        app.run(args);
+
+        Properties properties = new Properties();
+
+        try {
+            String fileName = ClassLoader
+                    .getSystemClassLoader().getResource("db_conn.properties")
+                    .getFile();
+
+            properties.load(new FileReader(fileName));
+
+            manager = new PostgresConnectionManager(properties);
+
+            UserRepository uRepo = new UserJDBCDao();
+            Users u = uRepo.findByEmail("hayden@gmail.com");
+
+            if(u != null) {
+                System.out.println(u);
+            } else {
+                System.out.println("No user found");
+            }
+            System.out.println();
+
+            u = new Users("e@gmail.com", "123", "1");
+            int newId = uRepo.save(u);
+            if(newId > 0) {
+                u.setId(newId);
+                System.out.println(u);
+            }
+            System.out.println();
+
+            List<Users> users = (List<Users>) uRepo.findAll();
+            for(Users user : users) {
+                System.out.println(user);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     }
 }
--- src/test/java/com/company/tests/services/depositTest.java
+++ src/test/java/com/company/tests/services/depositTest.java
@@ -1,28 +1,28 @@
-//package com.company.tests.services;
-//
-//import com.company.app.models.Account;
-//import com.company.app.services.AccountServices;
-//import com.company.app.services.AccountServices;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class depositTest {
-//
-//    Account account = new Account();
-//
-//    AccountServices balance;
-//
-//    @Before
-//    public void init() {
-//        balance = new AccountServices();
-//    }
-//
-//    @Test
-//    public void shouldUpdateBalance() {
-//        double expectedOutput = 5;
-//        account.setBalance(5)
-//        Assert.assertEquals("balance not updated", expectedOutput, account.getBalance());
-//
-//    }
-//}
+package com.company.tests.services;
+
+import com.company.app.models.Account;
+import com.company.app.services.AccountServices;
+import com.company.app.services.AccountServices;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class depositTest {
+
+    Account account = new Account();
+
+    AccountServices balance;
+
+    @Before
+    public void init() {
+        balance = new AccountServices();
+    }
+
+    @Test
+    public void shouldUpdateBalance() {
+        double expectedOutput = 5;
+        account.setBalance(5);
+        Assert.assertEquals("balance not updated", expectedOutput, account.getBalance());
+
+    }
+}
